a8239a7882f2caff282a6a79128faf997ce16f25
sp-darshan <spdarshan14@gmail.com>
2025-08-06 20:34:34 +0530

    Initial phase with Hero and Upload Page

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+
+import './index.css'  // ensure Tailwind is imported
+
+function App() {
+  return <>
+    <div className='min-h-screen w-full '><Home /></div>
+  </>
+}
+
+export default App
