a990af21580254c539b57d5ccba3429347acd21d
sp-darshan <spdarshan14@gmail.com>
2025-09-05 20:52:48 +0530

    Webhooks restructured

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import connectDB from './configs/mongodb.js';
-import userRouter from './routes/userRoutes.js';
+import userRouter from './routes/userRoute.js';
 
 // Create app
 const app = express();
