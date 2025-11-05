f8866287964d7e7216470c7c24c9909d6afe29b2
sp-darshan <spdarshan14@gmail.com>
2025-11-05 14:45:43 +0530

    Clerk issue in redirecting

--- client/src/pages/Hero.jsx
+++ client/src/pages/Hero.jsx
@@ -24,6 +24,7 @@ export default function Hero() {
   const [hovered, setHovered] = useState('center')
   const navigate = useNavigate()
   const { isSignedIn } = useUser()
+  const { openSignIn } = useClerk()
 
   const zMap = {
     center: 20,
